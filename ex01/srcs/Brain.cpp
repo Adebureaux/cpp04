f11cd22aabde60b629eb4f19c417dd4fba9fb5c3
Augustin Debureaux <adebureaux@gmail.com>
2022-04-15 17:00:05 +0200

    leaks

--- ex01/srcs/Brain.cpp
+++ ex01/srcs/Brain.cpp
@@ -9,17 +9,18 @@ Brain::Brain()
 
 Brain::Brain(const Brain &other)
 {
+	std::cout << "Default Brain copy constructor called" << std::endl;
 	*this = other;
 }
 
 Brain &Brain::operator=(const Brain &other)
 {
-
+	for (int i = 0; i < 100; i++)
+		this->ideas[i] = other.ideas[i];
 	return (*this);
 }
 
 Brain::~Brain()
 {
 	std::cout << "Default Brain constructor called" << std::endl;
-	delete[] this->ideas;
 }
--- ex01/srcs/main.cpp
+++ ex01/srcs/main.cpp
@@ -9,7 +9,7 @@ int main()
 		meta[i] = new Dog();
 	for (int i = 50; i < 100; i++)
 		meta[i] = new Cat();
-	for(int i = 0; i < 100; i++)
+	for (int i = 0; i < 100; i++)
 		delete meta[i];
 	return (0);
 }
